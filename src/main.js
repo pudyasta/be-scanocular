7bafaf6b6e3c4c2f9676adc31e565c16e817f836
pudyasta <pudyastasatria@gmail.com>
2023-07-12 17:04:43 +0700

    migrate db

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import logger from "./app/logging.js";
+import cors from "cors";
 import { web } from "./app/web.js";
 
-web.listen(3030, () => {
+web.use(cors());
+
+web.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+web.listen(8000, () => {
   logger.info("app strat");
 });
